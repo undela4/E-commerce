6e072a5fadd60e69017ced32550fbbed4cfd33cf
undela4 <muraliundela29@gmail.com>
2024-07-09 19:01:34 +0530

    final

--- E-commers/src/components/Footer/Error.jsx
+++ E-commers/src/components/Footer/Error.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const Error = () => {
+export  const Error = () => {
     return (
         <div className="container d-flex flex-column justify-content-center align-items-center vh-75 text-center mb-5">
     <img src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?size=338&ext=jpg&ga=GA1.1.2082370165.1716768000&semt=ais_user"/>
